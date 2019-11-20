0f83bad27399560c257ca38a8f4786e91d54c619
adlpnb <57916800+adlpnb@users.noreply.github.com>
2019-11-20 18:18:08 -0500

    Update MASTER_preprocess.m

--- madawaska_4tet2/movement_analysis/preprocess/MASTER_preprocess.m
+++ madawaska_4tet2/movement_analysis/preprocess/MASTER_preprocess.m
@@ -1,7 +1,7 @@
 %% Where's my data?
 plotting_video_flag = 0;
 plotting_flag = 1;
-data_folder = input('Type the path to your .tsv data files, i.e. ''D:\\str4tet_madawaska\\Data\''>>' );
+data_folder = input('Enter in ''/Users/emilywood/Desktop/MATLAB/madawaska_4tet/data''\n>>');
 fprintf('%s\n',['We''ll be looking inside ' data_folder])
 filenames = dir(fullfile(data_folder,'*.tsv'));
 
@@ -43,8 +43,35 @@ end
 
 %% Get proportion of NaNs for each participant and marker.
 % What to do with nan gaps? Decide depending on how broken is the data, 
-% fill the nan parts or cut them out, or the whole marker?
+% fill the nan parts or cut them out, or the whole marker?cDATA{tr}.X
 
+% Emily's matrix of NAN counts
+
+% make a matrix
+% the rows are the 83 markers
+% the columns are the 12 trials
+NAN_matrix = zeros(numel(DATA{1}.col_names), numel(filenames)); % This will store absolute count of NAN's
+NAN_matrix_prop = NAN_matrix; % This will store the proportion of NAN's
+
+for tr = 1:numel(DATA)
+    for marker=1:size(DATA{tr}.X,3)
+        NAN_matrix(marker, tr)=sum(isnan(DATA{tr}.X(:,1,marker))); % this is absolute count
+        NAN_matrix_prop(marker,tr)=sum(isnan(DATA{tr}.X(:,1,marker))/32738); %proportion
+    end
+end
+
+% ok there is a little bug in the code.. 
+% the markers are different for files 1-8 files than for files 9-12 (corresponds to columns) 
+
+% this is because the first 8 files are the full ensemble and the last 4 
+% files are the soloists. There are 83 markers for the full ensemble but 
+% less for each soloist (e.g. 19)
+
+% e.g. compare DATA{1}.col_names to DATA{12}.col_names
+
+% I don't think it matters though at this point. The soloist's markers will
+% start from row 1, but once they run out, the rest of the values will be
+% 0.
 
 %% What other movement variables with reduced dimension should we think of?
 % PCA? MSD? The thing from quaternions?
