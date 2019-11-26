00390e2d273477c866ed09bb664a12a0b07a845d
dobri <dobri.dotov@yandex.com>
2019-11-26 14:13:20 -0500

    cleaned and commented a little

--- orphx_sub/movement_analysis/a_master_analysis.m
+++ orphx_sub/movement_analysis/a_master_analysis.m
@@ -28,23 +28,20 @@ rez.v = nan(18*43,1);
 rez.pl = nan(18*43,1);
 rez.osc_prop = nan(18*43,1);
 
-% V Analyze per trial. tr=1;pp=1;
+% Analyze per trial. tr=1;pp=1;
 for tr=1:18
-    %discarded=[];
     for pp=1:43
         fprintf('%4.0f,%4.0f\n',tr,pp)
         rez.meta(((tr-1)*43+pp),:) = [tr pp ll.IDSnum(pp) abs(mod(tr,2)-1)+1];
         D=ll.D{tr}(:,:,pp);
-        % disp(size(D))
-        % V Skip if the nan prop > .5.
+        % Skip if the nan prop > .5.
         if sum(isnan(D(:,1)))/size(D,1)>.5
             continue
         end
-        % V Crop the nans. (Preserve the nan index?)
+        % Crop the nans. (Preserve the nan index?)
         D(sum(isnan(D),2)>0,:)=[];
         time=((1:size(D,1))')./ll.sf;
         
-
         % It seems like there isn't a great number of intermittent
         % nan-intervals within a participant's trial. Hence, don't worry
         % about a difficult stitching and cleaning procedure. Just crop nans.
@@ -86,18 +83,15 @@ for tr=1:18
             pause
         end
         
-        % V Oscillations analysis: Peak times, periods, and amplitudes.
-        % V The oscillation energy H (sum across both PCs).
-        % cycl.cycles, cycl.peak_loc, cycl.peak_amp, cycl.iois,
-        % cycl.amp_interp, cycl.freq_interp, cycl.h, cycl.q
+        % Oscillations analysis: Peak times, periods, and amplitudes.
+        % The oscillation energy H (sum across both PCs).
         mid_processing.cycl{tr,pp} = cycles_amp_freq_energy(temp(:,1:2),time,ll.sf,2);
         switch 2
             case 2
                 print('-djpeg','-r72',['raw_mov_amp_h_etc_TR' num2str(tr,'%02.0f') '_PP' num2str(pp,'%2.0f') '_' datestr(now,'yyyy-mm-dd-HHMMSS') '.jpeg'])
         end
 
-        % ! Insert an image-printing option for the raw cycle analysis.
-
+        % Summarize the mov variables as means or medians.
         if ~isempty(mid_processing.cycl{tr,pp})
             rez.q1((tr-1)*43+pp) = mean(mid_processing.cycl{tr,pp}.q(300:end,1));
             rez.q2((tr-1)*43+pp) = mean(mid_processing.cycl{tr,pp}.q(300:end,2));
@@ -110,9 +104,8 @@ for tr=1:18
             rez.pl((tr-1)*43+pp,1) = sum(dot(diff(D(300:end,:)),diff(D(300:end,:)),2).^.5)/(size(D,1)/ll.sf/60);
         end
 
-        
-        % V BPM mode. Pool pc1 and 2 together. If needed, the analysis can be split.
-        % V BPM as the location of the largest peak in the kde, from among both PC1 and PC2.
+        % BPM mode, as the location of the largest peak in the kde.
+        % Pool PC1 and 2 together. If needed, the analysis can be split.
         switch 1
             case 1
                 bpms = [60*mid_processing.cycl{tr,pp}.cycles{1}(:,2); 60*mid_processing.cycl{tr,pp}.cycles{2}(:,2)];
@@ -126,13 +119,7 @@ for tr=1:18
                     mid_processing.cycl{tr,pp}.tempo_stats{d} = tempo_modes_with_adaptive_kde(60*mid_processing.cycl{tr,pp}.cycles{d}(:,2),0);
                 end
         end
-        % Later, if it's necessary to show the bpm modes across trials and
-        % participants, find the following among the star scripts.
-        % fig_kde_prob_densities_tempo_nice(kde_tempos)
-        %print('-djpeg','-r600',['tempo_distr_kde_' datestr(now,'yyyy-mm-dd-HHMMSS') '.jpeg'])
-        %print('-depsc',['tempo_distr_kde_' datestr(now,'yyyy-mm-dd-HHMMSS') '.eps'])
     end
-    %    discarded
 end
 
 rez.all.labels = {'Trial','pp','ID','Cond','Amp','R2PC2byPC1','BPM-mode','H','q1','q2','Speed','PathLength','OscTimeProp'};
@@ -140,6 +127,7 @@ rez.all.data = [rez.meta rez.tempo_mode rez.pc2_over_pc1 rez.amp rez.h rez.q1 re
 for dv=5:13;subplot(3,3,dv-4);boxplot(rez.all.data(:,dv),rez.all.data(:,4));title(rez.all.labels{dv});end
 
 %{
+Useful commands to store the product of this as pic or binary data file.
 print('-djpeg','-r300',['rez_dvs_' datestr(now,'yyyy-mm-dd-HHMMSS') '.jpeg'])
 
 save(fullfile(pwd,['rez_' datestr(now,'yyyy-mm-dd-HHMMSS') '.mat']),'rez')
@@ -154,4 +142,13 @@ fprintf(fid,'%12.4f,',rez.all.data(k,:));
 fprintf(fid,'\n');
 end;
 fclose(fid);
+
+It would be useful to insert here an image-printing option for 
+the raw cycle analysis.
+
+Later, if it's necessary to show the bpm modes across trials and
+participants, find the following among the star scripts.
+fig_kde_prob_densities_tempo_nice(kde_tempos)
+print('-djpeg','-r600',['tempo_distr_kde_' datestr(now,'yyyy-mm-dd-HHMMSS') '.jpeg'])
+print('-depsc',['tempo_distr_kde_' datestr(now,'yyyy-mm-dd-HHMMSS') '.eps'])
 %}
