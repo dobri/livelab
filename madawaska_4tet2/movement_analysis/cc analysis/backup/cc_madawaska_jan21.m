dd3c70a8db963042780a1f4dc7bbc8bacf730695
adlpnb <57916800+adlpnb@users.noreply.github.com>
2021-02-02 18:32:25 -0800

    ran wcc, added r script for analyzing

--- madawaska_4tet2/movement_analysis/cc analysis/backup/cc_madawaska_jan21.m
+++ madawaska_4tet2/movement_analysis/cc analysis/backup/cc_madawaska_jan21.m
@@ -36,15 +36,12 @@ if strcmp(method_flag,'cc_and_gcorder')
         D{2}.X_processed_morder,D{2}.X_detrended_processed_morder];
 end
 
-dataTrajs={'X_clean_processed','X_detrended_processed','A'};
-
-
+dataTrajs={'X_processed','X_detrended_processed','A'};
 
 %% CC analysis - position data
 counter=0;
 for piecei = 1:numel(D)
     for traji = 1:numel(dataTrajs)
-        counter2=0;
         switch dataTrajs{traji}
             case 'A'
                 sr = sr100;
@@ -52,10 +49,11 @@ for piecei = 1:numel(D)
                 sr = sr8;
         end
         
-        % Make vector to store correlation coefficients
+        % Preallocate vector to store correlation coefficients
+        % cor_vals=zeros(6*size(D{piecei}.(dataTrajs{traji}),3),1,numel(D)); % there's something weird here.
         cor_vals = [];
         
-        % increment the counter
+        % Specify the same parameters that Andrew used
         counter = counter+1;
         
         switch method_flag
@@ -79,7 +77,6 @@ for piecei = 1:numel(D)
                     S = readtable(['p' num2str(piecei) 't' num2str(triali) '.txt']);
                     % S = readtable('p1t1.txt');
                     salient_points = S.Var1;
-                    salient_points_markers=S.Var3;
             end
             
             %take the maximum unsigned CC coefficient for each of the 6 possible
@@ -122,10 +119,6 @@ for piecei = 1:numel(D)
                                     stn = time_interpolate(salient_points,salient_points);
                                     
                                     %corrgram(D{piecei}.(dataTrajs{traji})(row,:,triali),D{piecei}.(dataTrajs{traji})(col,:,triali),maxlag,window,overlap)
-                                    if fcounter ==1
-                                        figure
-                                    end
-                                    
                                     subplot(num_trials,6,fcounter+6*(triali-1))
                                     %imagesc(tn,l,flip(wcc),[-1 1]);colorbar
                                     imagesc(t./sr,l,flip(wcc),[-1 1]);colorbar
@@ -154,28 +147,7 @@ for piecei = 1:numel(D)
                                     %    xlabel('Time [seconds or sections]')
                                     %end
                                     if triali==1
-                                        if row==1
-                                            row_lab='cello';
-                                        elseif row==2
-                                            row_lab='viola';
-                                        elseif row==3
-                                            row_lab='violin1';
-                                        else
-                                            row_lab='violin2';
-                                        end
-                                        
-                                        if col==1
-                                            col_lab='cello';
-                                        elseif col==2
-                                            col_lab='viola';
-                                        elseif col==3
-                                            col_lab='violin1';
-                                        else
-                                            col_lab='violin2';
-                                        end
-                                        %title(['Pair ' num2str(row) '-' num2str(col)])
-                                        title([row_lab '-' col_lab])
-
+                                        title(['Pair ' num2str(row) '-' num2str(col)])
                                     else
                                         title('')
                                     end
@@ -218,6 +190,79 @@ for p=1:2
     boxplot(D{p}.X_detrended_processed_wcc,reshape(meshgrid(1:8,1:6),1,[])')
 end
 
+%% CC Analysis - acceleration data
+counter=0;
+for piecei = 1:numel(D)
+    % Preallocate vector to store correlation coefficients
+    cor_vals=zeros(6*size(D{piecei}.A,2),1,numel(D));
+    
+    % Specify the same parameters that Andrew used
+    counter=counter+1;
+    
+    switch method_flag
+        case 'cc_and_gcorder'
+            maxlag = morders(counter); %lag is the same as the model order for gc
+            window=length(D{piecei}.A); %Whole length of the piece. Dobri suggests not to use this. Will discuss.
+            overlap=0; %No overlap
+        case 'wcc'
+            sr = 100; % Hz.
+            maxlag=round(max_lag*sr); % 2 seconds
+            window=round(win_len*sr); % 5 seconds
+            overlap=round(window/5); % half a window overlap
+    end
+    
+    for triali=1:size(D{piecei}.A,2)
+        %take the maximum unsigned CC coefficient for each of the 6 possible
+        %pairs of musicians for each trial
+        switch method_flag
+            case 'cc_and_gcorder'
+                % lag is the same as the model order for gc
+                % single trial-long window
+                mcounter = 0;
+                for row=1:4
+                    for col=1:4
+                        if col>row
+                            mcounter = mcounter + 1;
+                            [c,l]=xcov(D{piecei}.A(row,:,triali),D{piecei}.A(col,:,triali),morders(counter),'coef'); %only thing I'm not sure about is 'coef' - normalizes the sequence
+                            cor_vals(mcounter+6*(triali-1),1,piecei)=max(abs(c));
+                        end
+                    end
+                end
+            case 'wcc'
+                % max(A,[],'all') works from R2018b after.
+                fcounter = 0;
+                for row=1:4
+                    for col=1:4
+                        if col>row
+                            fcounter = fcounter + 1;
+                            [wcc,l,t]=corrgram(D{piecei}.A{triali}(row,:),D{piecei}.A{triali}(col,:),maxlag,window,overlap);
+                            cor_vals(fcounter+6*(triali-1),1,piecei)=max(max(abs(wcc)));
+                            if figs_flag
+                                subplot(2,3,fcounter)
+                                corrgram(D{piecei}.A{triali}(row,:),D{piecei}.A{triali}(col,:),maxlag,window,overlap)
+                                xtickangle(30)
+                                set(gca,'YTick',l(1:20:end))
+                                set(gca,'YTickLabel',l(1:20:end)./sr)
+                                ylabel('Lag, s')
+                                set(gca,'XTick',t(1:8:end))
+                                set(gca,'XTickLabel',round(t(1:8:end)./sr))
+                                xlabel('Time, s')
+                            end
+                        end
+                    end
+                end
+                if figs_flag
+                    if save_wcc_fig == 1 % print to file
+                        print(gcf,'-dpng','-r100','-loose',['wcc_' 'score' num2str(piecei) '_tr' num2str(triali) '_' datestr(now,'yymmdd-HHMMSS') '.png']);
+                    else
+                        pause % just inspect on the screen
+                    end
+                end
+        end
+    end
+    label_cc=['A_',method_flag];
+    D{piecei}.(label_cc)=cor_vals(:,:,piecei);
+end
 
 figure
 for p=1:2
@@ -229,19 +274,11 @@ end
 %% Save data
 if save_flag==1
     
-    %TURN THIS INTO A trajectory LOOP once I FIX the piece 2 labels
-    
-    %Position
-    
     %Reconfigure cor_vals
-    cor_vals_reconfig=[D{1}.X_clean_processed_wcc;D{2}.X_processed_wcc]; %fix this once I get piece 2 markers
+    cor_vals_reconfig=[D{1}.X_processed_cc;D{2}.X_processed_cc];
     
     %Make vector for pair
     pair=repmat([1:6]',16,1);
-    
-    %Make vector with the pair names
-    pair_names={'cello-viola','cello-v1','cello-v2','viola-v1','viola-v2','v1-v2'}';
-    pair_names=[pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names;pair_names]
     
     %Make vector for piece
     piece=repelem([1;2],48);
@@ -262,26 +299,13 @@ if save_flag==1
     condition=[condition1;condition2];
     
     
-    T=table(pair, pair_names, cor_vals_reconfig,condition,trial,trial_collapsed,piece);
-    filename='mada_cc_position.xlsx';
-    T.Properties.VariableNames = {'pair','pair_names', 'wcc', 'condition','trial', 'trial_collapsed','piece'};
+    T=table(pair, cor_vals_reconfig,condition,trial,trial_collapsed,piece);
+    filename='mada_cc3.xlsx';
+    T.Properties.VariableNames = {'pair','cc', 'condition','trial', 'trial_collapsed','piece'};
     writetable(T,filename);
-        
-    %Detrended
-    %Reconfigure cor_vals
-    cor_vals_reconfig_detrended=[D{1}.X_detrended_processed_wcc;D{2}.X_detrended_processed_wcc]; 
-    T2=table(pair, pair_names, cor_vals_reconfig_detrended,condition,trial,trial_collapsed,piece);
-    filename2='mada_cc_position_detrended.xlsx';
-    T2.Properties.VariableNames = {'pair','pair_names', 'wcc', 'condition','trial', 'trial_collapsed','piece'};
-    writetable(T2,filename2);
     
-    %Acceleration
-    cor_vals_reconfig_A=[D{1}.A_wcc;D{2}.A_wcc];
-    T3=table(pair, pair_names, cor_vals_reconfig_A,condition,trial,trial_collapsed,piece);
-    filename3='mada_cc_A.xlsx';
-    T3.Properties.VariableNames = {'pair','pair_names', 'wcc', 'condition','trial', 'trial_collapsed','piece'};
-    writetable(T3,filename3);
+    plot(trial,cor_vals_reconfig)
     
-
+    
 end
 
---
+++ madawaska_4tet2/movement_analysis/stats/cc/mada_cc.R
@@ -0,0 +1,265 @@
+#mada cc
+library(reshape2)
+library(ez)
+library(TOSTER)
+library(lsr)
+library(ggplot2)
+library(lme4)
+library(Rmisc)
+library(ggplot2)
+library(psych)
+library(wesanderson)
+library(lmerTest)
+
+##PREP
+setwd('/Users/emilywood/desktop/r/madawaska')
+imageDirectory<-"/Users/emilywood/desktop/r/madawaska'"
+options(max.print=1000000)
+
+
+##READ IN DATA
+cc_pos<-read.csv("mada_cc_position.csv", header = TRUE,na.strings = c("", "NA"))
+cc_pos_dt<-read.csv("mada_cc_position_detrended.csv", header = TRUE,na.strings = c("", "NA"))
+cc_a<-read.csv("mada_cc_A.csv", header = TRUE,na.strings = c("", "NA"))
+
+head(cc_pos)
+head(cc_pos_dt)
+head(cc_a)
+
+##FACTORING
+cc_pos$condition = factor(cc_pos$condition)
+cc_pos$piece<-factor(cc_pos$piece, ordered=TRUE)
+cc_pos$pair<-factor(cc_pos$pair, ordered=TRUE)
+
+cc_pos_dt$condition = factor(cc_pos_dt$condition,labels=c("Mechanical","Expressive"))
+cc_pos_dt$piece<-factor(cc_pos_dt$piece, ordered=TRUE)
+cc_pos_dt$pair<-factor(cc_pos_dt$pair, ordered=TRUE)
+
+cc_a$condition = factor(cc_a$condition)
+cc_a$piece<-factor(cc_a$piece, ordered=TRUE)
+cc_a$pair<-factor(cc_a$pair, ordered=TRUE)
+
+
+#First, we'll analyse acceleration
+#Dobri's plots showing individual slopes
+g <- vector("list",4)
+for (p in seq(1,2)) { #piece 1 and 2
+  for (c in seq(1,2)) { #condition 1 and 2
+    df<-cc_a[(cc_a$piece==p) & (cc_a$condition==c),]
+    #colors<-wes_palette("FantasticFox1",length(unique(df$pair)),type=("continuous"))
+    g[[p+2*(c-1)]] <- ggplot(df) + #plot gc against trial
+      geom_jitter(aes(x=trial, y=wcc, colour=pair), size=2, alpha=.8, height=.00, width=.0) +
+      geom_line(aes(x=trial, y=wcc, colour=pair), size=1.2, alpha=.5) +
+      #scale_colour_manual(values=colors) +
+      stat_summary(aes(x=trial,y=wcc), fun.y='mean', geom='line', size=1.2, alpha=.7) +
+      stat_summary(aes(x=trial,y=wcc), geom="ribbon", fun.data=mean_cl_boot, alpha=.5) +
+      #stat_summary(aes(x=trial,y=cc_a), geom="ribbon", fun.data=mean_se, alpha=.5) +
+      theme_classic() +
+      theme(axis.title = element_text(size=18),
+            axis.text = element_text(size=14, colour="black")) +
+      labs(x = "Trial", y = "Correlational-coupling", color="Musician pairing")+
+      if (c == 1) {
+        if (p==1){
+          scale_x_continuous(breaks = c(1, 3, 5,7), labels=c("1","2","3","4"))
+        } else {
+          scale_x_continuous(breaks = c(2, 4, 6,8), labels=c("1","2","3","4"))
+        }
+      } else if (c == 2) {
+        if (p==1){
+          scale_x_continuous(breaks = c(2, 4, 6,8), labels=c("1","2","3","4"))
+        }else{
+          scale_x_continuous(breaks = c(1, 3, 5,7),labels=c("1","2","3","4"))
+        }
+      }
+    head(df)
+  }
+}
+
+print(g[1]) #p1,c1 (m)
+print(g[2]) #p2,c1 (m)
+print(g[3]) #p1,c2 (e)
+print(g[4]) #p2,c2 (e)
+
+
+
+#PLOT
+
+mada1<-cc_a[which(cc_a$piece==1),]
+mada2<-cc_a[which(cc_a$piece==2),]
+
+#three ways of plotting
+
+(cc_p1_a<-ggplot(data=mada1, aes(x=trial, y=wcc, color=condition)) +
+    stat_summary(fun.y = mean, geom = "point", size=4)+
+    stat_summary(fun.y = mean, geom = "line", aes(group = condition),size=1.25) + 
+    stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) + 
+    scale_color_manual(values=c('firebrick4', 'goldenrod1'))+
+    labs(x = "Trial", y = "Correlational-coupling (r)", color="Playing style")+
+    #ggtitle("Effect of playing style over time on overall \ngranger-coupling (causal density)")+
+    theme_bw()+
+    scale_x_continuous(limits=c(1, 8), breaks=seq(1,8,1))+
+    #scale_y_continuous(limits=c(0.005, 0.015)) +
+    theme(panel.grid.major.x = element_line(),
+          panel.grid.major.y = element_line(),
+          plot.title = element_text(size = 22, vjust = 1.5),
+          axis.title = element_text( size=18),
+          legend.title = element_text(size = 18),
+          legend.key.size = unit(0.5, "cm"),
+          legend.text = element_text(size=16),
+          legend.background = element_rect(color = "black"),
+          axis.title.y = element_text(vjust= 1.8),
+          axis.title.x = element_text(vjust= -0.5),
+          axis.text = element_text(size=16, colour="black")))
+
+#ggsave("cc_p1.png", width=14, height=11)
+
+#same for piece 2
+(cc_p2_a<-ggplot(data=mada2, aes(x=trial, y=wcc, color=condition)) +
+    stat_summary(fun.y = mean, geom = "point", size=4)+
+    stat_summary(fun.y = mean, geom = "line", aes(group = condition),size=1.25) + 
+    stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) + 
+    scale_color_manual(values=c('firebrick4', 'goldenrod1'))+
+    labs(x = "Trial", y = "Correlational-coupling (r)", color="Playing style")+
+    #ggtitle("Effect of playing style over time on overall \ngranger-coupling (causal density)")+
+    theme_bw()+
+    scale_x_continuous(limits=c(1, 8), breaks=seq(1,8,1))+
+    #scale_y_continuous(limits=c(0.005, 0.015)) +
+    theme(panel.grid.major.x = element_line(),
+          panel.grid.major.y = element_line(),
+          plot.title = element_text(size = 22, vjust = 1.5),
+          axis.title = element_text( size=18),
+          legend.title = element_text(size = 18),
+          legend.key.size = unit(0.5, "cm"),
+          legend.text = element_text(size=16),
+          legend.background = element_rect(color = "black"),
+          axis.title.y = element_text(vjust= 1.8),
+          axis.title.x = element_text(vjust= -0.5),
+          axis.text = element_text(size=16, colour="black")))
+
+#ggsave("cc_p2.png", width=14, height=11)
+
+#LME for acceleration
+m00=lmer(wcc ~ 1 + (1|pair),data=cc_a,REML=0)
+m01=lmer(wcc ~ 1 + trial + (1|pair),data=cc_a,REML=0)
+m02=lmer(wcc ~ 1 + trial + piece + (1|pair),data=cc_a,REML=0) 
+m03=lmer(wcc ~ 1 + trial+condition + piece + (1|pair),data=cc_a,REML=0) 
+m04=lmer(wcc ~ 1 + trial*condition + piece + (1|pair),data=cc_a,REML=0) 
+m05=lmer(wcc ~ 1 + trial*condition*piece + (1|pair),data=cc_a,REML=0)  
+
+anova(m00,m01,m02,m03,m04,m05)
+
+summary(m02)
+
+
+
+#now analyse position
+
+g <- vector("list",4)
+for (p in seq(1,2)) { #piece 1 and 2
+  for (c in seq(1,2)) { #condition 1 and 2
+    df<-cc_pos[(cc_pos$piece==p) & (cc_pos$condition==c),]
+    #colors<-wes_palette("FantasticFox1",length(unique(df$pair)),type=("continuous"))
+    g[[p+2*(c-1)]] <- ggplot(df) + #plot gc against trial
+      geom_jitter(aes(x=trial, y=wcc, colour=pair), size=2, alpha=.8, height=.00, width=.0) +
+      geom_line(aes(x=trial, y=wcc, colour=pair), size=1.2, alpha=.5) +
+      #scale_colour_manual(values=colors) +
+      stat_summary(aes(x=trial,y=wcc), fun.y='mean', geom='line', size=1.2, alpha=.7) +
+      stat_summary(aes(x=trial,y=wcc), geom="ribbon", fun.data=mean_cl_boot, alpha=.5) +
+      #stat_summary(aes(x=trial,y=cc_pos), geom="ribbon", fun.data=mean_se, alpha=.5) +
+      theme_classic() +
+      theme(axis.title = element_text(size=18),
+            axis.text = element_text(size=14, colour="black")) +
+      labs(x = "Trial", y = "Correlational-coupling", color="Musician pairing")+
+      if (c == 1) {
+        if (p==1){
+          scale_x_continuous(breaks = c(1, 3, 5,7), labels=c("1","2","3","4"))
+        } else {
+          scale_x_continuous(breaks = c(2, 4, 6,8), labels=c("1","2","3","4"))
+        }
+      } else if (c == 2) {
+        if (p==1){
+          scale_x_continuous(breaks = c(2, 4, 6,8), labels=c("1","2","3","4"))
+        }else{
+          scale_x_continuous(breaks = c(1, 3, 5,7),labels=c("1","2","3","4"))
+        }
+      }
+    head(df)
+  }
+}
+
+print(g[1]) #p1,c1 (m)
+print(g[2]) #p2,c1 (m)
+print(g[3]) #p1,c2 (e)
+print(g[4]) #p2,c2 (e)
+
+
+
+#PLOT
+
+
+mada1<-cc_pos[which(cc_pos$piece==1),]
+mada2<-cc_pos[which(cc_pos$piece==2),]
+
+
+(cc_p1_pos<-ggplot(data=mada1, aes(x=trial, y=wcc, color=condition)) +
+    stat_summary(fun.y = mean, geom = "point", size=4)+
+    stat_summary(fun.y = mean, geom = "line", aes(group = condition),size=1.25) + 
+    stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) + 
+    scale_color_manual(values=c('firebrick4', 'goldenrod1'))+
+    labs(x = "Trial", y = "Correlational-coupling (r)", color="Playing style")+
+    #ggtitle("Effect of playing style over time on overall \ngranger-coupling (causal density)")+
+    theme_bw()+
+    scale_x_continuous(limits=c(1, 8), breaks=seq(1,8,1))+
+    #scale_y_continuous(limits=c(0.005, 0.015)) +
+    theme(panel.grid.major.x = element_line(),
+          panel.grid.major.y = element_line(),
+          plot.title = element_text(size = 22, vjust = 1.5),
+          axis.title = element_text( size=18),
+          legend.title = element_text(size = 18),
+          legend.key.size = unit(0.5, "cm"),
+          legend.text = element_text(size=16),
+          legend.background = element_rect(color = "black"),
+          axis.title.y = element_text(vjust= 1.8),
+          axis.title.x = element_text(vjust= -0.5),
+          axis.text = element_text(size=16, colour="black")))
+
+#ggsave("cc_p1.png", width=14, height=11)
+
+
+#same for piece 2
+(cc_p2_pos<-ggplot(data=mada2, aes(x=trial, y=wcc, color=condition)) +
+    stat_summary(fun.y = mean, geom = "point", size=4)+
+    stat_summary(fun.y = mean, geom = "line", aes(group = condition),size=1.25) + 
+    stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) + 
+    scale_color_manual(values=c('firebrick4', 'goldenrod1'))+
+    labs(x = "Trial", y = "Correlational-coupling (r)", color="Playing style")+
+    #ggtitle("Effect of playing style over time on overall \ngranger-coupling (causal density)")+
+    theme_bw()+
+    scale_x_continuous(limits=c(1, 8), breaks=seq(1,8,1))+
+    #scale_y_continuous(limits=c(0.005, 0.015)) +
+    theme(panel.grid.major.x = element_line(),
+          panel.grid.major.y = element_line(),
+          plot.title = element_text(size = 22, vjust = 1.5),
+          axis.title = element_text( size=18),
+          legend.title = element_text(size = 18),
+          legend.key.size = unit(0.5, "cm"),
+          legend.text = element_text(size=16),
+          legend.background = element_rect(color = "black"),
+          axis.title.y = element_text(vjust= 1.8),
+          axis.title.x = element_text(vjust= -0.5),
+          axis.text = element_text(size=16, colour="black")))
+
+#ggsave("cc_p2.png", width=14, height=11)
+
+#stats
+m00=lmer(wcc ~ 1 + (1|pair),data=cc_pos,REML=0)
+m01=lmer(wcc ~ 1 + trial + (1|pair),data=cc_pos,REML=0)
+m02=lmer(wcc ~ 1 + trial + piece + (1|pair),data=cc_pos,REML=0) 
+m03=lmer(wcc ~ 1 + trial+condition + piece + (1|pair),data=cc_pos,REML=0) 
+m04=lmer(wcc ~ 1 + trial*condition + piece + (1|pair),data=cc_pos,REML=0) 
+m05=lmer(wcc ~ 1 + trial*condition*piece + (1|pair),data=cc_pos,REML=0)  
+
+anova(m00,m01,m02,m03,m04,m05)
+
+
+
